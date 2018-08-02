82501be31eed471edb744b4b57f8c1c1caba79c4
Asperulla <40033339+Asperulla@users.noreply.github.com>
2018-08-02 20:25:49 +0200

    changed 1 bottle

--- hw_bottles.py
+++ hw_bottles.py
@@ -1,8 +1,10 @@
 
-for bottle in range(0, 99):
-	print (str(99 - bottle) + " bottles of beer on the wall, " + str(99 - bottle) + " bottles of beer."
+#99bottles
+for bottle in range(0, 98):
+	print (str(99 - bottle) + " bottles of beer on the wall, " + str(99 - bottle) + " bottles of beer. "
 		" Take one down and pass it around, " + str(99 - bottle - 1) + " bottles of beer on the wall."
 		)
 	bottle = bottle - 1
 	
+print("1 bottle of beer on the wall, 1 bottle of beer. Take one down and pass it around, no more bottles of beer on the wall.")
 print("No more bottles of beer on the wall, no more bottles of beer. Go to the store and buy some more, 99 bottles of beer on the wall.")
