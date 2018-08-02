3ebac6724d88b496823771e53a050b60b174ee94
Asperulla <40033339+Asperulla@users.noreply.github.com>
2018-08-02 20:10:32 +0200

    99 bottles uploaded

---
+++ hw_bottles.py
@@ -0,0 +1,8 @@
+
+for bottle in range(0, 99):
+	print (str(99 - bottle) + " bottles of beer on the wall, " + str(99 - bottle) + " bottles of beer."
+		"Take one down and pass it around, " + str(99 - bottle - 1) + " bottles of beer on the wall."
+		)
+	bottle = bottle - 1
+	
+print("No more bottles of beer on the wall, no more bottles of beer. Go to the store and buy some more, 99 bottles of beer on the wall.")
