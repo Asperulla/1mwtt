8fdc562d480f57cf57f386d947a7344ca2d8a314
Asperulla <40033339+Asperulla@users.noreply.github.com>
2018-08-02 20:39:24 +0200

    divided to better to read

--- hw_bottles.py
+++ hw_bottles.py
@@ -15,10 +15,9 @@ for bottle in range(0, 97):
 		" Take one down and pass it around, " + str(99 - bottle - 1) + " bottles of beer on the wall."
 		)
 	bottle = bottle - 1
-
 	
 print(
-	"2 bottles of beer on the wall, 2 bottles of beer. 
+	"2 bottles of beer on the wall, 2 bottles of beer.
       Take one down and pass it around, 1 bottle of beer on the wall.
       1 bottle of beer on the wall, 1 bottle of beer. Take one down and pass it around, 
       no more bottles of beer on the wall. 
