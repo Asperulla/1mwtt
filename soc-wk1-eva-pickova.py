72b72fcb27cdcfd0050c5e3b3bb2ec6c99b63acd
Asperulla <40033339+Asperulla@users.noreply.github.com>
2018-08-02 16:36:36 +0200

    day1 added

---
+++ soc-wk1-eva-pickova.py
@@ -0,0 +1,12 @@
+#Hours in a year. How many hours are in a year?
+print(365*24)
+#8760
+
+#Minutes in a decade. How many minutes are in a decade?
+common_year = 365*24*60
+leap_year = 366*24*60
+print(8*common_year + 2*leap_year)
+#5258880
+
+#Your age in seconds. How many seconds old are you?
+print(34*365*24*60*60)
