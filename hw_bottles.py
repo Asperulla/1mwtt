82f6e151ed905dff13d4eda9c0f98088ba06a70e
Asperulla <40033339+Asperulla@users.noreply.github.com>
2018-08-24 22:49:10 +0200

    rozdeleni radku

--- hw_bottles.py
+++ hw_bottles.py
@@ -19,8 +19,8 @@ for bottle in range(0, 97):
 print(
 	"2 bottles of beer on the wall, 2 bottles of beer.
       Take one down and pass it around, 1 bottle of beer on the wall.
-      1 bottle of beer on the wall, 1 bottle of beer. Take one down and pass it around, 
-      no more bottles of beer on the wall. 
-      No more bottles of beer on the wall, no more bottles of beer. 
-      Go to the store and buy some more, 99 bottles of beer on the wall."
+      \n 1 bottle of beer on the wall, 1 bottle of beer. \n Take one down and pass it around, 
+      \n no more bottles of beer on the wall. 
+      \n No more bottles of beer on the wall, no more bottles of beer. 
+      \n Go to the store and buy some more, 99 bottles of beer on the wall."
      )
